372cb4381a4946cc89bfd151bf4bee997ad78918
Gleb Tishchenko <owod1.0@gmail.com>
2022-01-21 12:42:16 +0200

    let there be weatherbot

---
+++ fetchweather/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests, json
+
+key = "ed71a4ff62e0d8b0edffad31710b4085"
+
+def getWeather(units, city):
+    city = city
+    units = units
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid={}".format(city, units, key)
+
+    response = requests.get(url)
+    data = json.loads(response.text)
+
+    return data
+
+def index(request):
+    weather = getWeather("metric", "Helsinki")
+    city = weather["name"]
+    temp = str(weather["main"]["temp"])
+    return HttpResponse("<h1>" + city + "</h1>" + temp)
