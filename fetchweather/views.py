2f118ddc6d5a9c176519b6b4ab4264aee6f3d85a
Gleb Tishchenko <tishgle@digitalentshelsinki.internal>
2022-02-21 15:44:04 +0200

    is to st

--- fetchweather/views.py
+++ fetchweather/views.py
@@ -8,8 +8,6 @@ appid = "ed71a4ff62e0d8b0edffad31710b4085"
 botid = "382694174:AAHZeoMmAJQ6C5oLQfoNax11deTbSC2gKvA"
 message = ""
 chatID = ""
-city = ""
-temp = 0
 
 def getWeather(units, city):
     city = city
@@ -37,6 +35,10 @@ def bot(request):
 
     messageText = "Temp in " + city + "is " + str(temp)
     chatID = str(data['message']['chat']['id'])
+
+    weather = getWeather("metric", "Helsinki")
+    city = weather["name"]
+    temp = str(weather["main"]["temp"])
 
     url = 'https://api.telegram.org/bot' + botid + '/sendMessage?chat_id=' + chatID + '&parse_mode=HTML&text=' + messageText
     m = requests.post(url)
