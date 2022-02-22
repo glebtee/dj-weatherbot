ecb1b6ea4e2591d6c9ab799e9bc2edd281e6fda1
Gleb Tishchenko <tishgle@digitalentshelsinki.internal>
2022-02-22 14:27:07 +0200

    modify publish profile

--- fetchweather/views.py
+++ fetchweather/views.py
@@ -6,8 +6,11 @@ from django.views.decorators.http import require_POST, require_GET
 
 appid = "ed71a4ff62e0d8b0edffad31710b4085"
 botid = "382694174:AAHZeoMmAJQ6C5oLQfoNax11deTbSC2gKvA"
+
 message = ""
 chatID = ""
+city = ""
+temp = ""
 
 def getWeather(units, city):
     city = city
@@ -23,6 +26,7 @@ def index(request):
     weather = getWeather("metric", "Helsinki")
     city = weather["name"]
     temp = str(weather["main"]["temp"])
+
     return HttpResponse("<h1>" + city + "</h1>" + "ilma njyttes: " + temp)
 
 @require_POST
