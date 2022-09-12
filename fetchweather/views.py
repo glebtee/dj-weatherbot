72b68ff88436cf21f7c28bc6f14baf8eb00036f0
Gleb Tishchenko <tishgle@digitalentshelsinki.internal>
2022-09-12 14:39:08 +0300

    return request arg back to cat

--- fetchweather/views.py
+++ fetchweather/views.py
@@ -72,7 +72,7 @@ def city(request, cityname):
     return HttpResponse(responsestr)
 
 # path to cat webpage
-def cat():
+def cat(request):
     """ returns page with a cat
     """
     caturl=getCatURL()
